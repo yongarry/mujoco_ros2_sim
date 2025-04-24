526f605987dd9c7375879b32054b7074ef070072
yongarry <jasonshin0537@snu.ac.kr>
2025-04-24 10:43:57 +0900

    tf bug fix

--- mujoco_ros/include/mjros.h
+++ mujoco_ros/include/mjros.h
@@ -25,7 +25,7 @@
 #include "std_msgs/msg/float32.hpp"
 #include "std_msgs/msg/float32_multi_array.hpp"
 #include "sensor_msgs/msg/joint_state.hpp"
-#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 #include <tf2/LinearMath/Quaternion.h>
 #include <tf2/LinearMath/Matrix3x3.h>
 
